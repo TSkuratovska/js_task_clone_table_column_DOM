6135e1595295dfe08f33eb20f6b62dd3382e10b5
Tetiana Skuratovska <tanya.skuratovskaya@gmail.com>
2024-10-11 14:40:27 +0200

    make build

---
+++ index.babe420b.js
@@ -0,0 +1,2 @@
+function e(e){for(var o=0;o<e.rows.length;o++){var r=e.rows[o],t=r.cells[1],l=r.cells[r.cells.length-1];if(t&&l){var c=t.cloneNode(!0);r.insertBefore(c,l)}}}var o=document.querySelector("thead"),r=document.querySelector("tbody"),t=document.querySelector("tfoot");e(o),e(r),e(t);
+//# sourceMappingURL=index.babe420b.js.map
